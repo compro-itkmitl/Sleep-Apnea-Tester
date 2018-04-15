135008d050fe00d98e3b9f2d59a259308bb3bf2e
Thitikorn Padungwatsawat <60070018@kmitl.ac.th>
2018-04-15 11:56:35 +0700

    BMI_Calculate : Edit grammar
    Change "Your are" to "You are"

--- Risk record/BMI Calculate/BMI_Calculate.c
+++ Risk record/BMI Calculate/BMI_Calculate.c
@@ -8,15 +8,15 @@ int main() {
 	scanf("%d", &age);
 	if (age < 18) {
 		printf("--------------------------------\n");
-		printf("   Your are not in criterion.   \n");
+		printf("    You are not in criterion.   \n");
 		printf("--------------------------------\n");
 		system("pause");
 		return 0;
 	}
 	float height, weight, bmi;
-	printf("Please enter your height(cm.).\n");
+	printf("Please enter your height(cm.)\n");
 	scanf("%f", &height);
-	printf("Please enter your weight(kg.).\n");
+	printf("Please enter your weight(kg.)\n");
 	scanf("%f", &weight);
 	height = height / 100;
 	bmi = weight / (height * height);
@@ -27,23 +27,23 @@ int result(float bmi) {
 	printf("--------------------------------\n");
 	printf("  BMI = %.2f\n", bmi);
 	if (bmi <= 18.5) {
-		printf("  Comment :	Your are underweight, you need to do more exercise and eat all 5 groups.\n");
+		printf("  Comment :	You are underweight, you need to do more exercise and eat all 5 groups.\n");
 		printf("  Risk 	  :	Malnutrition.\n");
 	}
 	else if (bmi > 18.5 && bmi <= 22.9) {
-		printf("  Comment : 	Your are normal.\n");
+		printf("  Comment : 	You are normal.\n");
 		printf("  Risk    : 	None\n");
 	}
 	else if (bmi > 22.9 && bmi <= 24.9) {
-		printf("  Comment : 	Your are overweight, please do exercise and bate greasy foods.\n");
+		printf("  Comment : 	You are overweight, please do exercise and bate greasy foods.\n");
 		printf("  Risk    :	None\n");
 	}
 	else if (bmi > 24.9 && bmi <= 29.9) {
-		printf("  Comment : 	Your are pre-obese, please do exercise and avoid greasy foods.\n");
+		printf("  Comment : 	You are pre-obese, please do exercise and avoid greasy foods.\n");
 		printf("  Risk    : 	High Blood Pressure and Gestational Diabetes\n");
 	}
 	else if (bmi > 29.9) {
-		printf("  Comment : 	Your are obese, please beware for obesity and do exercise with avoid greasy foods.\n");
+		printf("  Comment : 	You are obese, please beware for obesity and do exercise with avoid greasy foods.\n");
 		printf("  Risk    : 	High Blood Pressure, Gestational Diabetes and Obesity\n");
 	}
 	printf("--------------------------------\n");
