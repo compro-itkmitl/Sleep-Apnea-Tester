1157dee2b77a40697f631d4c1af851260fdda3c2
PrintTrd <60070034@kmitl.ac.th>
2018-04-18 02:29:01 +0700

    Add Risk factors and edit language

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
 ## Sleep Apnea Tester
-&nbsp;&nbsp;&nbsp;&nbsp;Sleep Apnea Tester is a micro-controller project for Computer Programming 2017 with two main objectives are Sleep Apnea surveillance and report irregularity statistics while sleeping.
+&nbsp;&nbsp;&nbsp;&nbsp;Sleep Apnea Tester is a micro-controller project for Computer Projramming 2017 with two main objectives are Sleep Apnea surveillance and report irregularity statistics while sleeping.
   
-&nbsp;&nbsp;&nbsp;&nbsp;Presently, the problem in sleeping was increase and these problems can risk assessment in diseases. Sleep Apnea Tester can solve sleep problem by respiratory rate measure, snore check and others while sleeping with Sound Microphone Detection Module. The statistics will be saved and can be checking to signify irregularity while sleeping if the irregularity was found while sleeping, the user can consult with paramedic for protecting in long term.
+&nbsp;&nbsp;&nbsp;&nbsp;Presently, the problem in sleeping was increase and these problems can risk assessment in diseases. Sleep Apnea Tester can solve about sleep problem by respiratory rate measure, snore check and others while sleeping with Sound Microphone Detection Module. The statistics will be saved and can be checking to signify about irregularity while sleeping, if the irregularity was found while sleeping, user can consult with paramedic for protect in long term.
 
 ## Installation
 1. **Download and Install Arduino IDE**
-  - Go to https://www.arduino.cc/ and click on download menu.
-  - Select computer's operating system for coding on Arduino IDE.
+  - Go to https://www.arduino.cc/ and clik on download menu.
+  - Select computer's operation system for coding on Arduino IDE.
   - Click on **JUST DOWNLOAD** for download program or **CONTRIBUTE & DOWNLOAD** for donate developer and download program.
   - Select folder for save program.
-  - Unzip the file and run arduino.exe. 
+  - Unzip file and run arduino.exe . 
 2. **Setting Arduino IDE**
   - Go to File >> Preference, it will show preference window.
   - Pasted http://arduino.esp8266.com/stable/package_esp8266com_index.json in **Additional Boards Manager URLs**.
   - Click "OK".
 3. **Download and Install Driver**
   - Go to https://www.silabs.com/products/development-tools/software/usb-to-uart-bridge-vcp-drivers.
-  - Select computer's operating system for download software.
-  - Install the driver on the computer.
+  - Select computer's operation system for download software.
+  - Install driver to computer.
 4. **Download and Install Library**
   - Go to https://github.com/compro-itkmitl/Sleep-Apnea-Tester/tree/master/Library
   - Download all ZIP file.
   - Open Arduino IDE and select Sketch >> Include Library >> Add .zip library.
   - Select ZIP file and click on "open".
-
+  
 ## Requirements
 - A Computer/PC/Laptop
 - Arduino UNO R3
--- Self-evaluation/Risk's Self-evaluation/Adultandchild.c
+++ Self-evaluation/Risk's Self-evaluation/Adultandchild.c
@@ -1,136 +1,160 @@
 #include<stdio.h>
-
+#include<stdlib.h>
+#include<string.h>
 int main() {
-	int age;
-	float point=0, height, weight, bmi;
-	char answer, sex[7];
-	printf("Please enter your age.\n");
-	scanf("%d", &age);
-	printf("Please enter your sex.(female/male)\n");
-	scanf("%s", sex);
-	if (sex=="female"){
-	    printf("Are you pregnent?.(y/n)\n");
-	    scanf("%c\n", &answer);
-	    if (answer=='y'){point++;}
-	}
-	if (sex=="male"){point++;}
-	if (age>=40){point++;}
-	if (age <= 20) {
-	    printf("1.Are you wet your bed at night?(y/n)\n");
-	    scanf("%c\n", &answer);
-	    if (answer=='y'){point+=0.5;}
-	    
-	    printf("2.Have you much night sweats?(y/n)\n");
-	    scanf("%c\n", &answer);
-	    if (answer=='y'){point++;}	    
+    int age, base;
+    float point=0, OSA=0, ABN=0, MIX=0, height, weight, bmi;
+    char answer, sex;
+    system("cls");
+    printf("Please enter your age.");
+    scanf(" %d", &age);
+    if (age>=40){point++;}
+    printf("\nPlease enter your sex?(f-female/m-male) ");
+    scanf(" %c", &sex);
+    system("cls");
+    if (sex=='f'){
+        printf("Are you pregnant?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){point++;}
+    }
+    else if (sex=='m'){point++;}
 
-	    printf("3.Are you breathing by mouth during the day?(y/n)\n");
-        scanf("%c\n", &answer);
-        if (answer=='y'){point+=2;}
-        
-	    printf("4.Do you often have a nightmare?(y/n)\n");
-        scanf("%c\n", &answer);
-        if (answer=='y'){point++;}        
-        
-	    printf("5.Are you naughty or fidget?(y/n)\n");
-        scanf("%c\n", &answer);
-        if (answer=='y'){point+=0.2;}  
-        
-	    printf("6.Are you not good or behide at study?(y/n)\n");
-        scanf("%c\n", &answer);
-        if (answer=='y'){point+=0.5;}  
-        
-        printf("---------------------RESULT----------------------\n");
-        printf(" Risk to have a sleep apnea disease : %.2f %%\n", (point/20)*100);
-		printf("   and you are not in criterion for check BMI.   \n");
-		printf("-------------------------------------------------\n");
-		return 0;
-// 		system("pause");
-	}
-    
-    else {
-	    printf("1.นอนหลับสนิทลำบากหรือไม่?(y/n)\n");
-        scanf(" %c", &answer);
-        if (answer=='y'){point++;}  
-        
-	    printf("2.ปัสสาวะบ่อยในตอนกลางคืน?(y/n)\n");
-        scanf(" %c", &answer);
-        if (answer=='y'){point+=0.5;}  
-        
-        printf("3.สะดุ้งผวา รู้สึกเหมือนขาดอากาหายใจไหม?(y/n)\n");
-        scanf(" %c", &answer);
-        if (answer=='y'){point+=2;}   
-        
-        printf("4.ปากแห้ง เจ็บคอขณะตื่นนอนใช่ไหม?(y/n)\n");
-        scanf(" %c", &answer);
-        if (answer=='y'){point+=2;}   
-        
-        printf("5.ปวดศีรษะในตอนเช้าใช่หรือไม่?(y/n)\n");
-        scanf(" %c", &answer);
-        if (answer=='y'){point+=1.5;}        
-    
-        printf("6.นอนเท่าไหร่ก็ไม่เพียงพอ?(y/n)\n");
-        scanf(" %c", &answer);
-        if (answer=='y'){point++;} 
-        
-        printf("7.มีอาการเผลอหลับ ขณะนั่งเงียบๆ?(y/n)\n");
-        scanf(" %c", &answer);
-        if (answer=='y'){point+=0.75;} 
-        
-        printf("8.มีอารมณ์แปรปรวน?(y/n)\n");
-        scanf(" %c", &answer);
+    printf("\nAre you colored people?(y/n) ");
+    answer = getchar();
+    if (answer=='y'){point++;}
+
+    printf("\nDo you allergic to something?(y/n) ");
+    answer = getchar();
+    if (answer=='y'){OSA++;}
+
+    if (age < 18) {
+        printf("\nAre you wet your bed at night?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){ABN+=0.5;}
+
+        printf("\nHave you massive perspire at night?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){point++;}
+
+        printf("\nAre you breathing by mouth instead of the nose during the day?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){point++;}
+
+        printf("\nDo you often have a nightmare?(y/n) ");
+        answer = getchar();
         if (answer=='y'){point+=0.5;}
         
-        printf("9.มีปัญหาเรื่องความจำ และสมาธิไหม?(y/n)\n");
-        scanf(" %c", &answer);
-        if (answer=='y'){point+=0.5;} 
+        printf("\nAre you Mischievously or fidget?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){point+=0.5;}
+
+        printf("\nAre you not good or behide at study?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){MIX+=0.5;}
+    }
+
+    else {
+        printf("\nDo you smoking?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){OSA++;}
         
-	    printf("Please enter your height(cm.)\n");
-	    scanf("%f", &height);
-	    printf("Please enter your weight(kg.)\n");
-	    scanf(" %f", &weight);
-	    height = height / 100;
-	    bmi = weight / (height * height);
-	    printf("---------------------RESULT----------------------\n");
-	    printf("  Age    = %d years old.\n", age);
-	    printf("  Height = %.2f cm.\n", height * 100);
-	    printf("  Weight = %.2f kg.\n", weight);
-	    printf("  BMI    = %.2f \n", bmi);
-	    if (bmi <= 18.5) {
-		    printf("  Comment :	You are underweight, you need to do more exercise and eat all 5 groups.\n");
-		    printf("  Risk 	  :	Malnutrition.\n");
-	    }
-	    else if (bmi > 18.5 && bmi <= 22.9) {
-		    printf("  Comment : 	You are normalweight.\n");
-		    printf("  Risk    : 	None\n");
-	    }
-	    else if (bmi > 22.9 && bmi <= 24.9) {
-		    printf("  Comment : You are overweight, please do exercise and bate greasy foods.\n");
-		    printf("  Risk    :	None\n");
-		    point++;
-	    }  
-    	else if (bmi > 24.9 && bmi <= 29.9) {
-		    printf("  Comment : You are pre-obese, please do exercise and avoid greasy foods.\n");
-		    printf("  Risk    : High Blood Pressure and Gestational Diabetes\n");
-		    point+=1.5;
-	    }
-	    else if (bmi > 29.9) {
-		    printf("  Comment : You are obese, please beware for obesity and do exercise with avoid greasy foods.\n");
-		    printf("  Risk    : High Blood Pressure, Gestational Diabetes and Obesity\n");
-		    point+=2;
-	    }
-	    printf("-------------------------------------------------\n");
-	    printf("------------------BMI CRITERION------------------\n");
-	    printf("      BMI < 18.50       :	Underweight\n");
-	    printf("      BMI 16.0 - 22.9   :	Normal\n");
-	    printf("      BMI 23.0 - 24.9   :	Overweight\n");
-	    printf("      BMI 25.0 - 29.9   :	Pre-obese\n");
-	    printf("      BMI 30.0 - 40.0   :	Obese\n");
-	    printf("      BMI 40.1 - 50.0   :	Morbid-obese\n");
-	    printf("      BMI > 50.0        :	Super-obese\n");
-	    printf("-------------------------------------------------\n");
-	    printf(" Risk to have a sleep apnea disease : %f%%\n", (point/18)*100);
-	    printf("-------------------------------------------------\n");
-	    return 0;
-	}
+        printf("\nDo you smoke opium?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){ABN++;}
+        
+        printf("\nDo you  drink alcohol?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){OSA++;}
+        
+        printf("\nDo you hard to deep sleep?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){point++;}  
+
+        printf("\nDo you frequent urination at night?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){ABN++;}  
+
+        printf("\nDo you shockingly like suffocate?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){MIX+=2;}   
+
+        printf("\nHave a headache in the morning?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){MIX++;}   
+
+        printf("\nDry mouth and sore throat when awake?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){point+=1.5;}        
+
+        printf("\nFall asleep while sitting in quietly place?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){point++;} 
+
+        printf("\nCan sleep at any time and not enough?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){point++;} 
+
+        printf("\nDo you have a problems with memory or concentration?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){MIX++;}
+
+        printf("\nChangeful mood?(y/n) ");
+        answer = getchar();
+        if (answer=='y'){MIX++;} 
+    }
+    system("cls");
+    printf("Please enter your height(cm.)\n");
+    scanf("%f", &height);
+    printf("\nPlease enter your weight(kg.)\n");
+    scanf(" %f", &weight);
+    height = height / 100;
+    bmi = weight / (height * height);
+
+    system("cls");
+    printf("---------------------RESULT----------------------\n");
+    printf("  Age    = %d years old.\n", age);
+    printf("  Height = %.2f cm.\n", height * 100);
+    printf("  Weight = %.2f kg.\n", weight);
+    printf("  BMI    = %.2f \n", bmi);
+    if (age<18){bmi-=2;}
+    printf("------------------BMI CRITERION------------------\n");
+    printf("      BMI < 18.50       :   Underweight\n");
+    printf("      BMI 16.0 - 22.9   :   Normal\n");
+    printf("      BMI 23.0 - 24.9   :   Overweight\n");
+    printf("      BMI 25.0 - 29.9   :   Pre-obese\n");
+    printf("      BMI 30.0 - 40.0   :   Obese\n");
+    printf("      BMI 40.1 - 50.0   :   Morbid-obese\n");
+    printf("      BMI > 50.0        :   Super-obese\n");
+    printf("-------------------------------------------------\n");
+    printf("-------------------------------------------------\n");
+    if (bmi <= 18.5) {
+        printf("  Comment : You are underweight, you need to do more exercise and eat all 5 groups.\n");
+        printf("  Risk    : Malnutrition.\n");
+    }
+    else if (bmi > 18.5 && bmi <= 22.9) {
+        printf("  Comment :     You are normalweight.\n");
+        printf("  Risk    :     None\n");
+    }
+    else if (bmi > 22.9 && bmi <= 24.9) {
+        printf("  Comment : You are overweight, please do exercise and bate greasy foods.\n");
+        printf("  Risk    : None\n");
+        OSA++;
+    }
+    else if (bmi > 24.9 && bmi <= 29.9) {
+        printf("  Comment : You are pre-obese, please do exercise and avoid greasy foods.\n");
+        printf("  Risk    : High Blood Pressure and Gestational Diabetes\n");
+        OSA+=1.5;
+    }
+    else if (bmi > 29.9) {
+        printf("  Comment : You are obese, please beware for obesity and do exercise with avoid greasy foods.\n");
+        printf("  Risk    : High Blood Pressure, Gestational Diabetes and Obesity\n");
+        OSA+=2;
+    }
+
+    if (age<18){base=2000;} else{base=8800;}
+    printf("%d\n", base);
+    printf(" Risk to have a sleep apnea disease : %f%%\n", (point+OSA+ABN+MIX/base)*100);
+    printf("-------------------------------------------------\n");
+    printf("-------------------------------------------------\n");
+    return 0;
 }
