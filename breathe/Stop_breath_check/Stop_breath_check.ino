0f379049f7c5fffcaf0c1ca6c98c9132fee5a2ab
Chaiyapong Nithipeerawat <Sullvo1147@hotmail.com>
2018-04-18 17:12:56 +0700

    fixed time duration error

--- breathe/Stop_breath_check/Stop_breath_check.ino
+++ breathe/Stop_breath_check/Stop_breath_check.ino
@@ -6,13 +6,13 @@
 
 
 //อย่าปรับความต้านทานของตัวสีฟ้า กว่าจะปรับได้ เหมือนจะขาดใจ
-//ใช้ได้ในเวลาสั้นๆ หานใช้นานค่าระยะเวลาที่หยุดหายใจจะเพี้ยน
 //in cast with normal breathing
 //anyway this code can't use in real situation
 int val = 0, i;
 int check_in_out = 0;
-int star_stop_breath;
+unsigned long star_stop_breath;
 File myFile;
+tmElements_t tm;
 
 // change this to match your SD shield or module;
 //     Arduino Ethernet shield: pin 4
@@ -28,17 +28,16 @@ void setup() {
 
   
   if (!SD.begin(chipSelect)) {
-    Serial.println("initialization failed!");
+    Serial.println("SD Card initialization failed!");
   }
   else{
-    Serial.println("initialization done.");
+    Serial.println("SD Card initialization done.");
   }
   myFile = SD.open("output.txt", FILE_WRITE);
 
 }
 void loop() {
-  
-tmElements_t tm;
+
 
 
 //  get input form voice detect for check breath
@@ -65,27 +64,11 @@ tmElements_t tm;
         print2digits(tm.Second);
         Serial.println();
 
-        if (myFile) {
-          write_in_file_2digits(tm.Hour);
-          myFile.write(':');
-          write_in_file_2digits(tm.Minute);
-          myFile.write(':');
-          write_in_file_2digits(tm.Second);
-          myFile.println();
-          }
       }
       Serial.print("you Stop breath for");
       continue_breath = millis() - star_stop_breath;
       Serial.print(continue_breath/1000);
       Serial.println("sec");
-
-      if (myFile) {
-        myFile.print("you Stop breath for");
-        myFile.print(continue_breath/1000);
-        myFile.println("sec");
-      // close the file:
-        myFile.close();
-        }
     }
     check_in_out = 0;
     star_stop_breath = 0;
@@ -115,22 +98,7 @@ tmElements_t tm;
         print2digits(tm.Second-1);
         Serial.print(" - ");
 
-        if (myFile) {
-          myFile = SD.open("test.txt");
-          myFile.print("Date = ");
-          myFile.print(tm.Day);
-          myFile.write('/');
-          myFile.print(tm.Month);
-          myFile.write('/');
-          myFile.print(tmYearToCalendar(tm.Year));
-          myFile.write(' ');
-          write_in_file_2digits(tm.Hour);
-          myFile.write(':');
-          write_in_file_2digits(tm.Minute);
-          myFile.write(':');
-          write_in_file_2digits(tm.Second-1);
-          myFile.print(" - ");
-          }
+
       }
       star_stop_breath = millis()-1000;
     }
