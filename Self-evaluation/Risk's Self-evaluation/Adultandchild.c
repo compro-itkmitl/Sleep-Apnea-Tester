f47f0c5cb6a324fd6cd21061dd5acc946a7db135
PrintTrd <60070034@kmitl.ac.th>
2018-04-18 10:31:12 +0700

    fixed input bugs
    Co-Authored-By: Thitikorn Padungwatsawat <60070018@kmitl.ac.th>

--- Self-evaluation/Risk's Self-evaluation/Adultandchild.c
+++ Self-evaluation/Risk's Self-evaluation/Adultandchild.c
@@ -2,120 +2,144 @@
 #include<stdlib.h>
 #include<string.h>
 int main() {
-    int age, base;
-    float point=0, OSA=0, ABN=0, MIX=0, height, weight, bmi;
+    int age;
+    float point=0, OSA=0, ABN=0, MIX=0, height, weight, bmi, base;
     char answer, sex;
     system("cls");
-    printf("Please enter your age.");
-    scanf(" %d", &age);
+    printf("Please enter your age.\n");
+    scanf("%d", &age);
+    getchar();
     if (age>=40){point++;}
-    printf("\nPlease enter your sex?(f-female/m-male) ");
-    scanf(" %c", &sex);
+    printf("\nPlease enter your sex?(f-female/m-male)\n");
+    scanf("%c", &sex);
+    getchar();
     system("cls");
     if (sex=='f'){
-        printf("Are you pregnant?(y/n) ");
-        answer = getchar();
+        printf("Are you pregnant?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){point++;}
     }
     else if (sex=='m'){point++;}
 
-    printf("\nAre you colored people?(y/n) ");
-    answer = getchar();
+    printf("\nAre you colored people?(y/n)\n");
+    scanf("%c", &answer);
+    getchar();
     if (answer=='y'){point++;}
 
-    printf("\nDo you allergic to something?(y/n) ");
-    answer = getchar();
+    printf("\nDo you allergic to something?(y/n)\n");
+    scanf("%c", &answer);
+    getchar();
     if (answer=='y'){OSA++;}
 
     if (age < 18) {
-        printf("\nAre you wet your bed at night?(y/n) ");
-        answer = getchar();
+        printf("\nAre you wet your bed at night?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){ABN+=0.5;}
 
-        printf("\nHave you massive perspire at night?(y/n) ");
-        answer = getchar();
+        printf("\nHave you massive perspire at night?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){point++;}
 
-        printf("\nAre you breathing by mouth instead of the nose during the day?(y/n) ");
-        answer = getchar();
+        printf("\nAre you breathing by mouth instead of the nose during the day?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){point++;}
 
-        printf("\nDo you often have a nightmare?(y/n) ");
-        answer = getchar();
+        printf("\nDo you often have a nightmare?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){point+=0.5;}
         
-        printf("\nAre you Mischievously or fidget?(y/n) ");
-        answer = getchar();
+        printf("\nAre you Mischievously or fidget?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){point+=0.5;}
 
-        printf("\nAre you not good or behide at study?(y/n) ");
-        answer = getchar();
+        printf("\nAre you not good or behide at study?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){MIX+=0.5;}
     }
 
     else {
-        printf("\nDo you smoking?(y/n) ");
-        answer = getchar();
+        printf("\nDo you smoking?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){OSA++;}
         
-        printf("\nDo you smoke opium?(y/n) ");
-        answer = getchar();
+        printf("\nDo you smoke opium?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){ABN++;}
         
-        printf("\nDo you  drink alcohol?(y/n) ");
-        answer = getchar();
+        printf("\nDo you  drink alcohol?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){OSA++;}
         
-        printf("\nDo you hard to deep sleep?(y/n) ");
-        answer = getchar();
+        printf("\nDo you hard to deep sleep?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){point++;}  
 
-        printf("\nDo you frequent urination at night?(y/n) ");
-        answer = getchar();
+        printf("\nDo you frequent urination at night?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){ABN++;}  
 
-        printf("\nDo you shockingly like suffocate?(y/n) ");
-        answer = getchar();
+        printf("\nDo you shockingly like suffocate?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){MIX+=2;}   
 
-        printf("\nHave a headache in the morning?(y/n) ");
-        answer = getchar();
+        printf("\nHave a headache in the morning?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){MIX++;}   
 
-        printf("\nDry mouth and sore throat when awake?(y/n) ");
-        answer = getchar();
+        printf("\nDry mouth and sore throat when awake?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){point+=1.5;}        
 
-        printf("\nFall asleep while sitting in quietly place?(y/n) ");
-        answer = getchar();
+        printf("\nFall asleep while sitting in quietly place?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){point++;} 
 
-        printf("\nCan sleep at any time and not enough?(y/n) ");
-        answer = getchar();
+        printf("\nCan sleep at any time and not enough?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){point++;} 
 
-        printf("\nDo you have a problems with memory or concentration?(y/n) ");
-        answer = getchar();
+        printf("\nDo you have a problems with memory or concentration?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){MIX++;}
 
-        printf("\nChangeful mood?(y/n) ");
-        answer = getchar();
+        printf("\nChangeful mood?(y/n)\n");
+        scanf("%c", &answer);
+        getchar();
         if (answer=='y'){MIX++;} 
     }
     system("cls");
     printf("Please enter your height(cm.)\n");
     scanf("%f", &height);
+    getchar();
     printf("\nPlease enter your weight(kg.)\n");
-    scanf(" %f", &weight);
+    scanf("%f", &weight);
     height = height / 100;
     bmi = weight / (height * height);
 
     system("cls");
     printf("---------------------RESULT----------------------\n");
-    printf("  Age    = %d years old.\n", age);
-    printf("  Height = %.2f cm.\n", height * 100);
-    printf("  Weight = %.2f kg.\n", weight);
-    printf("  BMI    = %.2f \n", bmi);
+    printf("      Age    = %d years old.\n", age);
+    printf("      Height = %.2f cm.\n", height * 100);
+    printf("      Weight = %.2f kg.\n", weight);
+    printf("      BMI    = %.2f \n", bmi);
     if (age<18){bmi-=2;}
     printf("------------------BMI CRITERION------------------\n");
     printf("      BMI < 18.50       :   Underweight\n");
@@ -151,9 +175,8 @@ int main() {
         OSA+=2;
     }
 
-    if (age<18){base=2000;} else{base=8800;}
-    printf("%d\n", base);
-    printf(" Risk to have a sleep apnea disease : %f%%\n", (point+OSA+ABN+MIX/base)*100);
+    if (age<18){base=20;} else{base=25;}
+    printf("  Risk to have a sleep apnea disease : %.4f%% \n", ((point+OSA+ABN+MIX)/base)*100);
     printf("-------------------------------------------------\n");
     printf("-------------------------------------------------\n");
     return 0;
