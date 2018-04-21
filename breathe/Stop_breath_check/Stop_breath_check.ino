3714bdf6ff12210664ed3c573a44f18adfd2995a
Chaiyapong Nithipeerawat <Sullvo1147@hotmail.com>
2018-04-22 01:26:05 +0700

    away clean file to create new file

--- breathe/Stop_breath_check/Stop_breath_check.ino
+++ breathe/Stop_breath_check/Stop_breath_check.ino
@@ -7,7 +7,7 @@
 
 //อย่าปรับความต้านทานของตัวสีฟ้า กว่าจะปรับได้ เหมือนจะขาดใจ
 //anyway this code can't use in real situation
-char command;
+String command;
 int val = 0, i;
 int check_in_out = 0;
 unsigned long star_stop_breath;
@@ -15,6 +15,7 @@ File myFile;
 tmElements_t tm;
 String printt;
 float time = 0.0, continue_breath;
+int f=0;
 
 
 // change this to match your SD shield or module;
@@ -26,6 +27,7 @@ const int chipSelect = 10;
 void setup() {
   Serial.begin(115200);
   pinMode(2,INPUT);
+  pinMode(3,INPUT);
   pinMode(SS, OUTPUT);
   Serial.println("--------Breathing Check for Sleep Apnea--------");
   
@@ -36,29 +38,54 @@ void setup() {
     Serial.println("--------  SD Card initialization done. --------");
   }
   int check_file = ReadText();
+
+      if(check_file != 0){
+        Serial.println("Something in \"output.txt\" file.");
+        SD.remove("output.txt");
+        Serial.println("Removing output.txt");
+        myFile = SD.open("output.txt", FILE_WRITE);
+      }
+//      else{
+//        myFile = SD.open("output.txt", FILE_WRITE);
+//      }
 //  if(check_file != 0){
-//    Serial.println("Something in \"output.txt\" file.");
-//    SD.remove("output.txt");
-//    Serial.println("Removing output.txt");
-//    myFile = SD.open("output.txt");
-//    myFile.close();
+//    Serial.println("Something in \"output.txt\" file. Do you want to delete it? y/n");
+//        while (!Serial.available()) {
+//                // read the incoming byte:
+//                command = Serial.readString();
+//                Serial.println(command);
+//                if(command == "y"){
+//                  Serial.println("Removing output.txt");
+//                  SD.remove("output.txt");
+//                  myFile = SD.open("output.txt");
+//                  myFile.close();
+//                  break;
+//                }
+//                else if(command == "n"){
+//                  Serial.println("Keep data in file");
+//                  myFile = SD.open("output.txt", FILE_WRITE);
+//                  myFile.close();
+//                  break;
+//                }
+//        }
 //  }
-  myFile = SD.open("output.txt", FILE_WRITE);
+
+
+//  myFile = SD.open("output.txt", FILE_WRITE);
 }
 void loop() {
 
-//  get input form voice detect for check breath
-  for(i=0; i<20; i++){
+
+  for(i=0; i<20; i++){               //  get input form voice detect for check breath
     val += digitalRead(2);
     delay(20);
   }
 
 
 
-//  check breath
-  if(val >= 17){
+
+  if(val >= 17){                    //  check breath
     check_in_out++;
-    //    Serial.print("in");
   }
 
   else{
@@ -77,8 +104,8 @@ void loop() {
 
 
 
-// check if don't breath out more than 4 sec(when check >8)
-  if(check_in_out==8){
+
+  if(check_in_out==8){           // check if don't breath out more than 4 sec(when check >8)
     printt = pre();
     star_stop_breath = millis()-1000;
   }
@@ -95,14 +122,17 @@ String print2digits(int number) {
   timed+=number;
   return timed;
 }
+
+
 void write_in_file_2digits(int number) {
   if (number >= 0 && number < 10) {
     myFile.write('0');
   }
   myFile.print(number);
 }
-int ReadText(){
-  // re-open the file for reading:
+
+
+int ReadText(){                                       // re-open the file for reading:
   int roundd=0;
   myFile = SD.open("output.txt");
   if (myFile) {
@@ -113,16 +143,16 @@ int ReadText(){
       Serial.write(myFile.read());
       roundd++;
     }
-    // close the file:
-    myFile.close();
+    myFile.close();                                   // close the file:
   } 
   else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening output.txt");
+    Serial.println("error opening output.txt");  // if the file didn't open, print an error:
   }
   Serial.println("-----------------------------------------------");
   return roundd;
 }
+
+
 String pre(){
   String timed = "";
   timed += "Stop breathing! ";
@@ -135,9 +165,9 @@ String pre(){
     timed += " ";
     timed += print2digits(tm.Hour);
     timed+=":";
-    timed+=tm.Minute;
+    timed+=print2digits(tm.Minute);
     timed+=":";
-    timed+=tm.Second-1;
+    timed+=print2digits(tm.Second-1);
     timed+=" - ";
   }
   return timed;
@@ -145,11 +175,11 @@ String pre(){
 String post(int num){
   String timed = "";
   if (RTC.read(tm)){
-        timed+=tm.Hour;
+        timed+=print2digits(tm.Hour);
         timed+=":";
-        timed+=tm.Minute;
+        timed+=print2digits(tm.Minute);
         timed+=":";
-        timed+=tm.Second;
+        timed+=print2digits(tm.Second);
       }
   timed+=" (you Stop breath for ";
   continue_breath = millis() - num;
@@ -164,8 +194,7 @@ void WriteText(String txt){
     myFile.close();
   } 
   else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening output.txt");
+    Serial.println("error opening output.txt");  // if the file didn't open, print an error:
   }
 }
 
